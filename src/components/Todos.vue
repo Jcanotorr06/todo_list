2701e9c97b93cdea45f02d99ddb9a198ab544c48
Jcanotorr06 <jcano062001@gmail.com>
2020-09-08 00:01:31 -0500

    Created Todo list

---
+++ src/components/Todos.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div v-for="todo in todos" v-bind:key="todo.id">
+      <Item v-bind:todo='todo' v-on:del-todo="$emit('del-todo', todo.id)"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Item from './Item.vue'
+export default {
+  name: "Todos",
+  components: {
+    Item
+  },
+  props: ["todos"]
+}
+</script>
+
+<style scoped>
+</style>
